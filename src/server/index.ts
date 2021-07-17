386444afbe7016d5248a822a9a329ce3ce7eaaba
Sergey Rudenko <srudenko@more.tv>
2021-07-17 15:25:56 +0300

    INIT: set server bundling with webpack and typescript compiler

---
+++ src/server/index.ts
@@ -0,0 +1,13 @@
+import { createServer } from "http";
+
+const port = process.env.PORT || 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello, World!</h1>");
+});
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port} / mode: ${process.env.NODE_ENV}`);
+});
---
+++ src/webpack.config.ts
@@ -0,0 +1,28 @@
+const path = require("path");
+
+enum ENV {
+  PROD = "production",
+  DEV = "development",
+}
+
+const isDev = process.env.NODE_ENV === ENV.DEV;
+
+module.exports = {
+  target: "node",
+  mode: isDev ? ENV.DEV : ENV.PROD,
+  entry: {
+    server: path.resolve(__dirname, "server/index.ts"),
+  },
+  output: {
+    path: path.resolve("build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
