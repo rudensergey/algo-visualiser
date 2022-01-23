d9ec1d45faba144e856a914a77fc7234f55d86e9
Sergey Rudenko <srudenko@more.tv>
2022-01-24 00:55:42 +0300

    add dropdown

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -7,33 +7,54 @@ import * as React from "react";
 import { TDropdown } from "./types";
 
 // Utils
-import { getUniqueId } from "../../utils/common";
 
 // Style
 import "./style.css";
 import { SUPPORTED_ALGORITMS } from "../visualiser/types";
 
 export const Dropdown: TDropdown = ({ list, classNames, onChange }) => {
-  const [id, dropdownId] = getUniqueId();
   const [algorithm, setAlgorithm] = React.useState(list[0]);
-  const [hidden, setHidedStatus] = React.useState(true);
+  const [hidden, setHiddenStatus] = React.useState(true);
+  const [coordinates, setCoordinates] = React.useState({ x: 0, y: 0 });
 
-  const showList = () => setHidedStatus(false);
-  const hideList = () => setHidedStatus(true);
-  const setValue = (value: SUPPORTED_ALGORITMS) => () => setAlgorithm(value);
+  const buttonRef = React.useRef(null);
+  const listRef = React.useRef(null);
+
+  const toogleList = () => {
+    if (hidden) {
+      const { bottom, left, width } = buttonRef.current.getBoundingClientRect();
+
+      setCoordinates({
+        x: left + width / 2 - 100,
+        y: bottom + document.documentElement.scrollTop,
+      });
+      setHiddenStatus(false);
+    } else {
+      setHiddenStatus(true);
+    }
+  };
+
+  const setValue = (value: SUPPORTED_ALGORITMS) => () => {
+    setAlgorithm(value);
+    onChange(value);
+    toogleList();
+  };
 
   return (
     <>
       <button
-        id={id}
+        ref={buttonRef}
         className={"dropdown__button" + ` ${classNames}`}
-        onMouseOver={showList}
-        onMouseLeave={hideList}
+        onClick={toogleList}
       >
         {algorithm + " sort"}
       </button>
       {!hidden && (
-        <ul id={dropdownId} className={"dropdown__list"}>
+        <ul
+          ref={listRef}
+          style={{ top: coordinates.y + "px", left: coordinates.x + "px" }}
+          className={"dropdown__list"}
+        >
           {list.map((value) => (
             <li onClick={setValue(value)}>{`${value} sort`}</li>
           ))}
