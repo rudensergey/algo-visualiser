3505edcb1001000355f10780b5d058a69e39d3ab
Sergey Rudenko <srudenko@more.tv>
2022-02-14 12:04:09 +0300

    refactor: add precalculations

--- src/components/_shared/Bar/Bar.tsx
+++ src/components/_shared/Bar/Bar.tsx
@@ -4,8 +4,9 @@ import React from "react";
 // Types
 import { BAR, IBarProps } from "./Bar.types";
 
-const Bar: React.FC<IBarProps> = ({ value, selected }) => (
-  <div style={{ height: value * 2 + "%" }} className={selected ? BAR.SELECTED : BAR.BAR}></div>
-);
+const Bar: React.FC<IBarProps> = ({ value, selected }) => {
+  const height = React.useMemo(() => value * 2 + "%", [value]);
+  return <div style={{ height }} className={selected ? BAR.SELECTED : BAR.BAR}></div>;
+};
 
 export default Bar;
--- src/components/_shared/Vertex/Vertex.tsx
+++ src/components/_shared/Vertex/Vertex.tsx
@@ -5,28 +5,24 @@ import React from "react";
 import { VERTEX_STATUS } from "@templates/Graph/Graph.types";
 import { IVertexProps, VERTEX } from "./types";
 
-const Vertex: React.FC<IVertexProps> = ({ statusModificator, pressedKey, row, column, changeStatus }) => {
+const Vertex: React.FC<IVertexProps> = ({ statusModificator: modificator, pressedKey, row, column, changeStatus }) => {
+  const classNames = React.useMemo(() => VERTEX.VERTEX + (modificator ? `--${modificator}` : ""), [modificator]);
+
   const onMouseOver = (event) => {
     event.preventDefault();
 
     if (!pressedKey) return;
 
-    if (statusModificator === null) changeStatus(column, row, VERTEX_STATUS.BLOCKED);
+    if (modificator === null) changeStatus(column, row, VERTEX_STATUS.BLOCKED);
     else changeStatus(column, row, null);
   };
 
   const onMouseDown = () => {
-    if (statusModificator === null) changeStatus(column, row, VERTEX_STATUS.BLOCKED);
+    if (modificator === null) changeStatus(column, row, VERTEX_STATUS.BLOCKED);
     else changeStatus(column, row, null);
   };
 
-  return (
-    <div
-      onMouseOver={onMouseOver}
-      onMouseDown={onMouseDown}
-      className={VERTEX.VERTEX + (statusModificator ? `--${statusModificator}` : "")}
-    ></div>
-  );
+  return <div onMouseOver={onMouseOver} onMouseDown={onMouseDown} className={classNames}></div>;
 };
 
 export default Vertex;
