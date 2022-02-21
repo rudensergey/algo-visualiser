8186b3f1c38a24ebb8c5f763000c43f6ca89494e
Sergey Rudenko <srudenko@more.tv>
2022-02-21 20:14:36 +0300

    refactor: add extend auth template

--- src/components/_templates/Auth/Auth.tsx
+++ src/components/_templates/Auth/Auth.tsx
@@ -1,23 +1,56 @@
 // Absolute imports
-import React from "react";
+import React, { FormEvent } from "react";
 
 // Components
 import Button from "@shared/Button";
+
+// Hooks
+import useInput from "@hooks/useInput";
 
 // Types
 import { AUTH } from "./Auth.types";
 import { BUTTON_TYPE } from "@shared/Button/Button.types";
 
 const AuthTemplate = () => {
+  const { value: username, reset: resetUserName, bind: bindUserName } = useInput();
+  const { value: password, reset: resetPassword, bind: bindPassword } = useInput();
+  const [loading, setLoading] = React.useState(false);
+
+  const onSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (loading) return;
+
+    setLoading(true);
+
+    const responce = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (responce.ok) {
+      const data = await responce.json();
+      console.log(data);
+      resetUserName();
+      resetPassword();
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div className={AUTH.WRAPPER}>
       <p>Auth</p>
       <div>
-        <form>
-          <input type="text"></input>
-          <input type="text"></input>
-          <button onClick={(event) => event.preventDefault()}>Submit!</button>
-        </form>
+        {loading ? (
+          <p>Fetching</p>
+        ) : (
+          <form onSubmit={onSubmit}>
+            <input type="text" {...bindUserName} />
+            <input type="text" {...bindPassword} />
+            <Button>Submit!</Button>
+          </form>
+        )}
       </div>
       <Button href="/" asHref="/" type={BUTTON_TYPE.GREEN}>
         &lt; Back
